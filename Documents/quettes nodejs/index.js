7b47a7330f85f92a9db1b5948d443668c247095c
kouaouci <karimakouaouci2803@gmail.com>
2021-10-13 22:11:54 +0200

    creation fichier html

---
+++ Documents/quettes nodejs/index.js
@@ -0,0 +1,8 @@
+
+const name = karima;
+const campus = lyon;
+
+const userInfo = require('./informations.js');
+
+console.log('hello i am ${userInfo.name} from ${userInfo.campus}');var cowsay = require("cowsay");
+
---
+++ Documents/quettes nodejs/informations.js
@@ -0,0 +1,8 @@
+
+const name = karima;
+const campus = lyon;
+
+wilder ={ name:karima ,
+          campus: lyon, };
+ module.exports = {name: karima,
+campus: lyon,}         
